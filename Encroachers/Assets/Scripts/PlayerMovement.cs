c09b3a3e15bf05ae0c71d7bbef309ec18f161df2
IronManhood <ironmanhood.dev@gmail.com>
2018-03-10 02:24:55 -0500

    Worked AI, look and movement scripts.

--- Encroachers/Assets/Scripts/PlayerMovement.cs
+++ Encroachers/Assets/Scripts/PlayerMovement.cs
@@ -4,21 +4,147 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    // ===== Public Vars
     public float speed = 10.0f;
+    public float jumpForce = 20.0f;
 
-    private Rigidbody rb;
+    public Camera fpsCamera;
+
+
+    // ===== Private Vars
+    private bool isOnGround = true;
+    private bool isFirstJump = false;
+
+    private int jumpState = 0;
+
+    private float distToGround;
+    private float rotY;
+    private float maxSpeed;
+    
+
+    private Rigidbody thisrb;
+    private Collider thiscldr;
+    private CameraLook camlook;
+
+    private Vector3 vel;
 
 	// Use this for initialization
 	void Start () {
-        rb = GetComponent<Rigidbody>();
-	}
+        thisrb = GetComponent<Rigidbody>();
+        thiscldr = GetComponent<Collider>();
+        camlook = fpsCamera.GetComponent<CameraLook>();
+
+        distToGround = thiscldr.bounds.extents.y;
+        rotY = transform.rotation.eulerAngles.y;
+
+        ToggleCursorState();
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        float moveZ = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToggleCursorState();
+        }
 
-        transform.Translate(moveX, 0, moveZ);
-	}
+        if (isOnGround == true)
+        {
+            // Get input.
+            vel.x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+            vel.z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+            // Move the player.
+            transform.Translate(vel);
+        }
+        else
+        {
+            // Move the player.
+            //transform.Translate(vel);
+            transform.position += vel;
+        }
+
+
+        //vel.x = Input.GetAxis("Horizontal") * Time.deltaTime * speed * 200.0f;
+        //vel.z = Input.GetAxis("Vertical") * Time.deltaTime * speed * 200.0f;
+
+        //thisrb.AddRelativeForce(vel);
+
+        //if(thisrb.velocity.magnitude > )
+
+        // Rotate the player on the Y euler axis.
+        rotY += Input.GetAxis("Mouse X") * camlook.mouseSensitivity * Time.deltaTime;
+        Quaternion localRotation = Quaternion.Euler(0.0f, rotY, 0.0f);
+        transform.rotation = localRotation;
+
+        HandleJump();
+    }
+
+
+
+
+    /* ===================================================== *
+     *  -This function handles jumping.-
+     *====================================================== */
+    private void HandleJump()
+    {
+
+        if (IsGrounded() == true)
+        {
+            isOnGround = true;
+            jumpState = 0;
+        }
+
+        switch (jumpState)
+        {
+            case 0:
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    Jump();
+                    jumpState = 1;
+                    isOnGround = false;
+                }
+                break;
+            case 1:
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    Jump();
+                    jumpState = 2;
+                }
+                break;
+            case 2:
+                break;
+        }
+        print("Func: " + IsGrounded());
+        print("Var: " + isOnGround);
+        print(jumpState);
+        
+    }
+
+    // Apply a force in the upward direction. aka Jump.
+    private void Jump()
+    {
+        thisrb.AddForce(transform.up * jumpForce * Time.deltaTime * 980.0f);
+    }
+
+    // Cast a ray in the negative up direction to test if the player is on the ground.
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
+
+     // Toggle the cursor visibility.
+     private void ToggleCursorState()
+    {
+        if(Cursor.visible == true)
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
 }
