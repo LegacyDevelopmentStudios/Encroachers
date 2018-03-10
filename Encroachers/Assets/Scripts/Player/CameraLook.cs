4ac9dc4e5dee666844912a9609adeccb1033a2e9
Sutalite <32411868+Sutalite@users.noreply.github.com>
2018-03-10 16:58:26 +0100

    Shooting networked
    Player can shoot and a cube spawn for all the players

---
+++ Encroachers/Assets/Scripts/Player/CameraLook.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CameraLook : NetworkBehaviour
+{
+    public GameObject PlayerObject;
+
+    public float mouseSensitivity = 100.0f;
+    public float clampAngle = 80.0f;
+    
+    private float rotX = 0.0f; // rotation around the right/x axis
+
+    bool localCam = false;
+
+    void Start()
+    {
+        rotX = transform.localRotation.eulerAngles.x;
+        localCam = transform.parent.GetComponent<NetworkIdentity>().isLocalPlayer;
+    }
+
+    void Update()
+    {
+        if (!localCam)
+            return;
+        float mouseY = -Input.GetAxis("Mouse Y");
+        
+        rotX += mouseY * mouseSensitivity * Time.deltaTime;
+        
+        // Clamp vertical rotation.
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        //Rotate the camera.
+        Quaternion localRotation = Quaternion.Euler(rotX, PlayerObject.transform.rotation.eulerAngles.y, 0.0f);
+        transform.rotation = localRotation;
+    }
+
+    public float GetMouseSensitivity()
+    {
+        return mouseSensitivity;
+    }
+}
---
+++ Encroachers/Assets/Scripts/Player/ShootManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ShootManager : NetworkBehaviour{
+
+    public Transform cam;
+    public GameObject cube;
+    
+    public void Start() {
+        cam = transform.GetChild(0);
+    }
+
+    void Update () {
+		if(Input.GetMouseButtonDown(0) && isLocalPlayer) {
+            Shoot();
+        }
+	}
+    
+    void Shoot() {
+        RaycastHit hit;
+        Debug.DrawLine(cam.position, cam.forward * 100, Color.red, 3f);
+        if (Physics.Raycast(cam.position, cam.forward, out hit)) {
+            CmdSpawnObject(hit.point);
+        }
+    }
+
+    [Command]
+    void CmdSpawnObject(Vector3 pos) {
+        GameObject go = GameObject.Instantiate(cube, pos, Quaternion.identity);
+        NetworkServer.Spawn(go);
+    }
+
+}
