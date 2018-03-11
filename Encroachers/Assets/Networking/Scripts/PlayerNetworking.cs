e1fef0302753869a5fef94f075b5d7c52317e92e
jettman100 <jettfuel77@gmail.com>
2018-03-11 03:43:14 -0600

    Multiplayer First Pass finished

---
+++ Encroachers/Assets/Networking/Scripts/PlayerNetworking.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Networking
+{
+	public class PlayerNetworking : MonoBehaviour 
+	{
+		[SerializeField] private GameObject playerCamera;
+		[SerializeField] private MonoBehaviour[] playerControlScripts;
+
+		private PhotonView photonView;
+
+		private void Start()
+		{
+			photonView = GetComponent<PhotonView>();
+
+			Initialize();
+		}
+
+		private void Initialize()
+		{
+			if (photonView.isMine)
+			{
+
+			}
+
+			else
+			{
+				playerCamera.gameObject.SetActive(false);
+
+				foreach(MonoBehaviour m in playerControlScripts)
+				{
+					m.enabled = false;
+				}
+			}
+		}
+	}
+}
