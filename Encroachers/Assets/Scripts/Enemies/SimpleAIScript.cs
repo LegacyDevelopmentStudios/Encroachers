0e6d8451a2e7a1bf078f9a0e53b1d483a64bc1ba
IronManhood <ironmanhood.dev@gmail.com>
2018-03-12 05:08:48 -0400

    Added SpawnerController script. (not networked)

--- Encroachers/Assets/Scripts/Enemies/SimpleAIScript.cs
+++ Encroachers/Assets/Scripts/Enemies/SimpleAIScript.cs
@@ -2,27 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Photon;
 
-public class SimpleAIScript : MonoBehaviour {
+public class SimpleAIScript : Photon.MonoBehaviour {
 
     public float agentSpeed = 2.5f;
 
-    public GameObject gobj;
+    private GameObject plr;
+    private Rigidbody rb;
+
+    private float lifeTime = 120f;
+
+    //private PhotonPlayer[] plrList;
+
+    //private PhotonPlayer target;
 
 	// Use this for initialization
 	void Start () 
     {
-		
+        rb = GetComponent<Rigidbody>();
+        //plrList = PhotonNetwork.otherPlayers;
+        plr = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        lifeTime -= Time.deltaTime;
 
-        Vector3 direction = gobj.transform.position - transform.position;
+        if (lifeTime <= 0f)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 direction = plr.transform.position - transform.position;
         
         direction.y = 0.0f;
 
-        transform.LookAt(gobj.transform.position, Vector3.up);
-        transform.Translate(direction.normalized * agentSpeed * Time.deltaTime, Space.World);
+
+        transform.LookAt(plr.transform.position, Vector3.up);
+        //transform.Translate(direction.normalized * agentSpeed * Time.deltaTime, Space.World);
+
+        if (rb.velocity.magnitude < agentSpeed)
+        {
+            rb.AddForce(direction.normalized * Time.deltaTime * rb.mass * 10f, ForceMode.VelocityChange);
+        }
 	}
+
+
+
+    //private PhotonPlayer GetClosestPlayer()
+    //{
+    //    PhotonPlayer closest;
+    //    float[] dist = new float[plrList.Length];
+
+    //    for (int i = 0; i < plrList.Length - 1; i++)
+    //    {
+    //        dist[i] = Vector3.Distance(transform.position, plrList[i].)
+    //    }
+
+    //    return closest;
+    //}
 }
---
+++ Encroachers/Assets/Scripts/Spawners/SpawnerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon;
+
+public class SpawnerController : Photon.MonoBehaviour {
+    // Public vars
+    public float lifeTime = 5f;
+    public float frequency = 0.95f;
+
+    public Text debugText;
+
+    
+
+    // Private vars
+    private float freqCounter = 0f;
+    
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lifeTime -= Time.deltaTime;
+        freqCounter += Time.deltaTime;
+
+        debugText.text = GetDebugString();
+
+        if (freqCounter >= frequency)
+        {
+            freqCounter = 0f;
+            // instantiate enemy
+            Vector3 newV = new Vector3(0f, 1.8f, 0f);
+            GameObject faggot = (GameObject)Instantiate(Resources.Load("EnemyBOT"), transform.position + newV, Quaternion.identity);
+        }
+
+        if (lifeTime <= 0f)
+        {
+            debugText.text = "";
+            Destroy(gameObject);
+        }
+	}
+
+
+
+    private string GetDebugString()
+    {
+        return "lifeTime: " + lifeTime.ToString() + NL() + 
+                "freqCounter: " + freqCounter.ToString();
+    }
+
+    private string NL() { return "\n"; }
+}
