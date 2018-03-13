cdf6c1020cca6227bb497330002f7dbede397932
jettman100 <jettfuel77@gmail.com>
2018-03-12 21:01:54 -0600

    Second Player First Pass

---
+++ Encroachers/Assets/Scripts/Player/Player2Shoot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Shoot : MonoBehaviour 
+{
+
+	public Camera cam;
+
+	void Update()
+	{
+		if(Input.GetMouseButton(0))
+		{
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit, 100))
+			{
+				Debug.Log(hit.collider.name);
+			}
+		}
+	}
+}
